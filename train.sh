97d4346fac39076fe6eec8aa5fbd67831baf2d2e
Marcos Baez <mbaezpy@gmail.com>
2019-02-20 16:45:23 +0100

    Added list intents

--- train.sh
+++ train.sh
@@ -1,2 +1,2 @@
 python -m rasa_nlu.train -c config/helloname_config.yml --data data/helloname --project helloname --verbose --path projects
-python -m rasa_nlu.train -c config/conversation_config.yml --data data/conversation --project conversation --verbose --path projects
+python -m rasa_nlu.train -c config/components_config.yml --data data/components --project components --verbose --path projects
